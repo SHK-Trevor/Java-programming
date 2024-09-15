05f325c5b4f7ad1bd6bec31b8090b5b8fb1bdefa
Trevor <trevormyles1@gmail.com>
2024-09-14 22:12:36 -0400

    Lab 1

---
+++ TrevorMyles_COMP228Lab1/src/exercise1/Singer.java
@@ -0,0 +1,90 @@
+package exercise1;
+
+public class Singer {
+    private int id;
+    private String name;
+    private String address;
+    private String dateOfBirth;
+    private int numberOfAlbums;
+
+    // Constructor 1
+    public Singer() {
+    }
+
+    // Constructor 2
+    public Singer(int id) {
+        this.id = id;
+    }
+
+    // Constructor 3
+    public Singer(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // Constructor 4
+    public Singer(int id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    // Constructor 5
+    public Singer(int id, String name, String address, String dateOfBirth) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    // Constructor 6
+    public Singer(int id, String name, String address, String dateOfBirth, int numberOfAlbums) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.dateOfBirth = dateOfBirth;
+        this.numberOfAlbums = numberOfAlbums;
+    }
+
+    //Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setNumberOfAlbums(int numberOfAlbums) {
+        this.numberOfAlbums = numberOfAlbums;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public int getNumberOfAlbums() {
+        return numberOfAlbums;
+    }
+}
+
